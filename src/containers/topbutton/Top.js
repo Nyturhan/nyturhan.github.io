35bdd5dc5fc15e43ca243c977272c01c2b6206d3
Nyturhan <nyturhan@gmail.com>
2023-01-02 01:27:57 +0300

    .

--- src/containers/topbutton/Top.js
+++ src/containers/topbutton/Top.js
@@ -26,7 +26,7 @@ export default function Top() {
   // When the user clicks on the button, scroll to the top of the document
   return (
     <button onClick={TopEvent} id="topButton" title="Go to top">
-      <i className="fa-solid fa-arrow-up-long" aria-hidden="true"></i>
+      <i className="fas fa-arrow-up" aria-hidden="true"></i>
     </button>
   );
 }
